0b506ade0b01a7c3afc9f397a2a5818aec533afb
Alexander <112755279+alexanderqmv@users.noreply.github.com>
2023-06-06 09:46:46 -0700

    Added `is_chain_valid` method

--- rs/rust_blockchain.rs
+++ rs/rust_blockchain.rs
@@ -1,12 +1,7 @@
 use crypto::digest::Digest;
 use crypto::sha2::Sha256;
 
-/* Preview:
-Block #0 [Hash: 86cd09c1a55aabfb11a3056cfabcf46654191974f9a1073776aae8f8c90f1df7][Prev Hash: 0]
-Block #1 [Hash: 6f0831242aecb5ee9a58668c46297c4be348424652f6a74d22aaeb5c916d87d5][Prev Hash: 86cd09c1a55aabfb11a3056cfabcf46654191974f9a1073776aae8f8c90f1df7]
-Block #2 [Hash: b7755f9a55ec5fc2b880cef05a3b22c8cc945ff863c95f5d6d62542f76bebe0f][Prev Hash: 6f0831242aecb5ee9a58668c46297c4be348424652f6a74d22aaeb5c916d87d5]   
-*/
-
+#[derive(Debug)]
 struct Block {
     index: u32,
     timestamp: i64,
@@ -16,7 +11,7 @@ struct Block {
 }
 
 impl Block {
-    fn new(index: u32, timestamp: i64, data: String, prev_hash: String) -> Block {
+    pub fn new(index: u32, timestamp: i64, data: String, prev_hash: String) -> Self {
         let mut block = Block {
             index,
             timestamp,
@@ -24,34 +19,33 @@ impl Block {
             prev_hash,
             hash: String::new(),
         };
-        block.hash = block.calculate_hash();
+
+        block.hash = block.calc_hash();
         block
     }
 
-    fn calculate_hash(&mut self) -> String {
+    fn calc_hash(&self) -> String {
         let mut hasher = Sha256::new();
         let input = format!(
             "{}{}{}{}",
-            self.index, self.timestamp, self.data, self.prev_hash
+            self.data, self.index, self.prev_hash, self.timestamp
         );
         hasher.input_str(&input);
         hasher.result_str()
     }
 }
-
-struct Blockchain {
+#[derive(Debug)]
+struct BlockChain {
     chain: Vec<Block>,
 }
-
-impl Blockchain {
-    fn new() -> Blockchain {
-        let genesis_block = Block::new(0, 0, "Genesis Block".to_string(), String::from("0"));
-        Blockchain {
-            chain: vec![genesis_block],
+impl BlockChain {
+    pub fn new() -> Self {
+        let genesis = Block::new(0,0,"Genesis Block".to_string(),"0".to_string());
+        BlockChain {
+            chain: vec![genesis],
         }
     }
-
-    fn add_block(&mut self, data: String) {
+    pub fn add_block(&mut self, data: String) {
         let prev_block = self.chain.last().unwrap();
         let new_block = Block::new(
             prev_block.index + 1,
@@ -59,19 +53,27 @@ impl Blockchain {
             data,
             prev_block.hash.clone(),
         );
-        self.chain.push(new_block);
+        self.chain.push(new_block);    
+    }
+    pub fn is_chain_valid(&self) -> bool {
+        for (i, block) in self.chain.iter().enumerate() {
+            if i > 0 {
+                let prev_block = &self.chain[i - 1];
+                if block.prev_hash != prev_block.hash {
+                    return false;
+                }
+            }
+            
+            if block.hash != block.calc_hash() {
+                return false;
+            }
+        }
+        return true;
     }
 }
-
 fn main() {
-    let mut blockchain = Blockchain::new();
-    blockchain.add_block("Transaction 1".to_string());
-    blockchain.add_block("Transaction 2".to_string());
-
-    for block in blockchain.chain {
-        println!(
-            "Block #{} [Hash: {}][Prev Hash: {}]",
-            block.index, block.hash, block.prev_hash
-        );
-    }
+    let mut blockchain = BlockChain::new();
+    blockchain.add_block("Some data".to_string());
+    blockchain.add_block("More data".to_string());
+    println!("Is chain valid? : {}",blockchain.is_chain_valid());
 }
